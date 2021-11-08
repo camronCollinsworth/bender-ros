102d749afbf185063e61fb9090c7d6d626961950
Matthew Oberg <matthew@oberg.us>
2021-11-08 20:05:38 +0000

    Fixing so it runs

--- bender_navsensors/src/imu.py
+++ bender_navsensors/src/imu.py
@@ -4,9 +4,10 @@ import rospy
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Imu
+from scipy import integrate
 
 msg = Float64MultiArray()
-tolerance = 2  ## sets tolerance, or acceptable jump in data values between clock cycles
+tolerance = 1   ## sets tolerance, or acceptable jump in data values between clock cycles
 distance_x = Float64()
 distance_y = Float64()
 velo_x = Float64()
@@ -50,6 +51,9 @@ def callback(data):
         ## filters out acceleration data that is too small ie. within margin of accepted error
        if (data.linear_acceleration.x > 0.1 or data.linear_acceleration.x < -0.1):
           accel_x.data = data.linear_acceleration.x
+          
+    else:
+        accel_x.data = last_x_accel
 
     ## only accepts data that is within a given tolerance level between clock cycles ie. if jump is too big, data is thrown out
     if (abs(data.linear_acceleration.y - last_y_accel) < tolerance):
@@ -58,16 +62,14 @@ def callback(data):
         if (data.linear_acceleration.y > 0.1 or data.linear_acceleration.y < -0.1):
           accel_y.data = data.linear_acceleration.y
 
+    else:
+        accel_y.data = last_y_accel
 
     ## sets acceleration data to be used for next cycle filtering
-    last_x_accel = float(accel_x.data * 0.1)
-    last_y_accel = float(accel_y.data * 0.1)
+    last_x_accel = accel_x.data
+    last_y_accel = accel_y.data
 
-    velo_x.data = float(velo_x.data + (accel_x.data * 0.1))
-    velo_y.data = float(velo_y.data + (accel_y.data * 0.1))
-
-    distance_x.data = float(distance_x.data + velo_x.data * 0.1)
-    distance_y.data = float(distance_y.data + velo_y.data * 0.1)
+    
 
     rospy.loginfo(rospy.get_caller_id() + "Velo x: [{}]\nVelo y:[{}]\nDist x:[{}]\nDist y:[{}]".format(velo_x.data, velo_y.data, distance_x.data, distance_y.data))
 
