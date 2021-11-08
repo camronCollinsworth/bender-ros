3c9ed31059c12b0f3ec1e176200566c1afd43353
camronCollinsworth <camroncollinsworth@gmail.com>
2021-11-07 19:41:13 -0800

    cleaned up more logic erros and added more descriptive variable names

--- bender_navsensors/src/imu.py
+++ bender_navsensors/src/imu.py
@@ -11,8 +11,8 @@ distance_x = Float64()
 distance_y = Float64()
 velo_x = Float64()
 velo_y = Float64()
-last_x = None
-last_y = None
+last_x_accel = None
+last_y_accel = None
 
 def callback(data):
     global msg
@@ -38,30 +38,30 @@ def callback(data):
     accel_y.data = float(0)
 
     ## check if data for last acceleration values is uninitialized, if so, set it to 0
-    if last_x is None:
-        last_x = float(0)
-    if last_y is None:
-        last_y = float(0)
+    if last_x_accel is None:
+        last_x_accel = float(0)
+    if last_y_accel is None:
+        last_y_accel = float(0)
 
 
     ## only accepts data that is within a given tolerance level between clock cycles ie. if jump is too big, data is thrown out
-    if ((abs(data.linear_acceleration.x) - abs(last_x)) < tolerance or (abs(data.linear_acceleration.x) - abs(last_x)) > (tolerance*-1)):
+    if (abs(data.linear_acceleration.x - last_x_accel) < tolerance):
 
         ## filters out acceleration data that is too small ie. within margin of accepted error
        if (data.linear_acceleration.x > 0.1 or data.linear_acceleration.x < -0.1):
           accel_x.data = data.linear_acceleration.x
 
     ## only accepts data that is within a given tolerance level between clock cycles ie. if jump is too big, data is thrown out
-    if ((abs(data.linear_acceleration.y) - abs(last_y)) < tolerance or (abs(data.linear_acceleration.y) - abs(last_y)) > (tolerance*-1)):
+    if (abs(data.linear_acceleration.y - last_y_accel) < tolerance):
       
     ## filters out acceleration data that is too small ie. within margin of accepted error
         if (data.linear_acceleration.y > 0.1 or data.linear_acceleration.y < -0.1):
           accel_y.data = data.linear_acceleration.y
 
 
-    ## sets data to be used for next cycle
-    last_x = float(accel_x.data * 0.1)
-    last_y = float(accel_y.data * 0.1)
+    ## sets acceleration data to be used for next cycle filtering
+    last_x_accel = float(accel_x.data * 0.1)
+    last_y_accel = float(accel_y.data * 0.1)
 
     velo_x.data = float(velo_x.data + (accel_x.data * 0.1))
     velo_y.data = float(velo_y.data + (accel_y.data * 0.1))
