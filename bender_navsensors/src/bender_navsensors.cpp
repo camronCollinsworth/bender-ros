82e84b51bfed75f5b511051d5ba56cf0197ef08e
Matthew Oberg <matthew@oberg.us>
2022-04-29 12:32:43 -0600

    GPS/IMU stuff

--- bender_navsensors/src/bender_navsensors.cpp
+++ bender_navsensors/src/bender_navsensors.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/State.h>
-#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
 
 mavros_msgs::State state;
 void get_state(const mavros_msgs::State::ConstPtr& msg)
@@ -19,18 +19,26 @@ int main (int argc, char **argv)
 
 	ros::ServiceClient arming = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
 
-	ros::Publisher state_pub = nh.advertise<mavros_msgs::State>("bender_navsensors/ns_state", 10);
+	ros::Publisher armed_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/enabled", 10);
+	ros::Publisher conn_pub = nh.advertise<std_msgs::Bool>("bender_navsensors/state/connected", 10);
 
-	ros::Rate loop_rate(20);
+        ros::Rate loop_rate(20); 
 
 	mavros_msgs::CommandBool arm;
 	arm.request.value = true;
 
+	std_msgs::Bool armed;
+	std_msgs::Bool conn;
+
 	while (ros::ok())
 	{
-		state_pub.publish(state);
+		armed.data = state.armed;
+		conn.data = state.connected;
 
-		if (!state.armed)
+		armed_pub.publish(armed);
+		conn_pub.publish(conn);
+
+		if (!armed.data)
 		{
 			arming.call(arm);
 		}
